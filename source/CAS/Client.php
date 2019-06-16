e1a90088c1caec5fd180beebcbae3449dfbecbd1
Han <596813350@qq.com>
2019-06-16 19:20:17 +0800

    大小写修改

--- source/CAS/Client.php
+++ source/CAS/Client.php
@@ -340,7 +340,7 @@ class CAS_Client
         phpCAS::traceBegin();
         // the URL is build only when needed
         if ( empty($this->_server['login_url']) ) {
-            $this->_server['login_url'] = $this->_buildQueryUrl($this->_getServerBaseURL().'login','service='.urlencode(C("cas_callback")));
+            $this->_server['login_url'] = $this->_buildQueryUrl($this->_getServerBaseURL().'login','service='.urlencode(C("CAS_CALLBACK")));
         }
         $url = $this->_server['login_url'];
         if ($renew) {
